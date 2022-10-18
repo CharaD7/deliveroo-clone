c267eafb82a588f934281bb6d3eb12dc1b0e8660
CharaD7 <jijakahn6@gmail.com>
2022-10-18 15:48:29 +0000

    Finished search field

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,8 @@
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import {
-  UserIcon,
-  ChevronDownIcon,
-  SearchIcon,
-  AdjustmentIcon
-} from 'react-native-heroicons/outline';
+import * as Icons from 'react-native-heroicons/outline';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -24,20 +19,34 @@ const HomeScreen = () => {
       <Text className="text-red-500">
 
         {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2" >
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4" >
           <Image
             source={{ uri: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png' }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
 
-          <View className='flex-1 w-72 mr-5' >
+          <View className='flex-1 w-72 ' >
             <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
             <Text className='font-bold text-xl'>Current Location
-              <ChevronDownIcon size={20} color='#00CCBB' />
+              <Icons.ChevronDownIcon size={15} color='#00CCBB' />
             </Text>
           </View>
 
-          <UserIcon size={35} color='#00CCBB'/>
+          <Icons.UserIcon size={35} color='#00CCBB'/>
+        </View>
+
+        {/* Search */}
+        <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+          <View className='flex-row flex-1 ml-4 bg-gray-200 space-x-2 p-3' style={{ width: 332 }}>
+            <Icons.MagnifyingGlassIcon color='gray' size={20} />
+            <TextInput
+              placeholder="Restuarants and cuisines"
+              keyboardType='default'
+              className='-mt-1'
+            />
+          </View>
+
+          <Icons.AdjustmentsVerticalIcon color='#00CCBB'/>
         </View>
 
       </Text>
